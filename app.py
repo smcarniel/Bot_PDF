7a435d845e39768a705ff9979bb14632329b9fd0
smcarniel <39606896+smcarniel@users.noreply.github.com>
2025-05-02 19:12:34 -0300

    app.py

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ import streamlit as st
 from utils import extract_text_from_pdf, split_text, create_vector_store
 from langchain.chains.question_answering import load_qa_chain
 from langchain_openai import OpenAI
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 st.set_page_config(page_title="PDF Chatbot")
 
@@ -20,10 +16,10 @@ if pdfs and query:
         for pdf in pdfs:
             all_text += extract_text_from_pdf(pdf)
         chunks = split_text(all_text)
-        db = create_vector_store(chunks, os.getenv("OPENAI_API_KEY"))
+        db = create_vector_store(chunks, st.secrets["OPENAI_API_KEY"])
         docs = db.similarity_search(query)
 
-        llm = OpenAI(temperature=0, openai_api_key=os.getenv("OPENAI_API_KEY"))
+        llm = OpenAI(temperature=0, openai_api_key=st.secrets["OPENAI_API_KEY"])
         chain = load_qa_chain(llm, chain_type="stuff")
         response = chain.run(input_documents=docs, question=query)
 
